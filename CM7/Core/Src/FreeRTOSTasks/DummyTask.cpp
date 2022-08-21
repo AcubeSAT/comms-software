bdabdb65e00c0c6425cbeaa5b86de40f4a596603
konkapo <kostaskapogl@gmail.com>
2022-08-21 15:37:00 +0300

    change freeRTOSConfig.h to an STM32H745 one

--- CM7/Core/Src/FreeRTOSTasks/DummyTask.cpp
+++ CM7/Core/Src/FreeRTOSTasks/DummyTask.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 #include "stm32h7xx_hal.h"
 
-void DummyTask::execute() {
-    uint8_t counter = 0;
-
-    for (;;) {
-        char str[100];
-        UART_HandleTypeDef huart3;
-        uint32_t e = 1;
-        sprintf(str ,"Time needed to modulate 200 kbits %d ms\r\n", counter);
-        HAL_UART_Transmit(&huart3, reinterpret_cast<const uint8_t *>(str), sizeof(str), 100);
-        vTaskDelay(pdMS_TO_TICKS(10));
-        counter++;
-    }
-
-}
+//void DummyTask::execute() {
+//    uint8_t counter = 0;
+//
+//    for (int i=0; i<100; i++) {
+//        char str[100];
+//        UART_HandleTypeDef huart3;
+//        uint32_t e = 1;
+//        sprintf(str ,"Time needed to modulate 200 kbits %d ms\r\n", counter);
+//        HAL_UART_Transmit(&huart3, reinterpret_cast<const uint8_t *>(str), sizeof(str), 100);
+//        vTaskDelay(pdMS_TO_TICKS(10));
+//        counter++;
+//    }
+//
+//}
--- CM7/Core/Src/main_cpp.cpp
+++ CM7/Core/Src/main_cpp.cpp
@@ -10,15 +10,35 @@ template<class T>
 static void vClassTask(void *pvParameters) {
     (static_cast<T *>(pvParameters))->execute();
 }
+void vTaskCode( void * pvParameters ) {
+    /* The parameter value is expected to be 1 as 1 is passed in the
+    pvParameters value in the call to xTaskCreate() below.
+
+*/  configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
+    uint8_t t = 0;
+    while(t<100)
+    {
+        t++;
+        /* Task code goes here. */
+    }
+}
 
 extern "C" void main_cpp(){
 
-    xTaskCreate(vClassTask<DummyTask>, "Dummy", 2000, nullptr, tskIDLE_PRIORITY + 1, NULL);
+    BaseType_t xReturned;
+    TaskHandle_t xHandle = NULL;
+    xReturned = xTaskCreate(vTaskCode, "Dummy", 20000, nullptr, tskIDLE_PRIORITY + 1, &xHandle );
+    if( xReturned == pdPASS )
+    {
+        /* The task was created.  Use the task's handle to delete the task. */
+        vTaskDelete( xHandle );
+    }
+
     vTaskStartScheduler();
 
-    while (true) {
-
-
-    }
+//    while (true) {
+//
+//
+//    }
     return;
 }
