7ffd550ed157b4fb149f47fdeb0f3fa709ec7251
George Kikas <kikasgeorg@ece.auth.gr>
2022-09-30 23:24:02 +0000

    Fix submodules!

--- Core/lib/Data-Link-Layer/inc/CCSDSLogger.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-template <class ΜyNotif>
-void ccsdsLogNotice(TxRx txRx, NotificationType notificationType, ΜyNotif Notif);
--- Core/lib/Physical-Layer/inc/etl_profile.h
+++
@@ -1,8 +0,0 @@
-#ifndef ETL_PROFILE_H
-#define ETL_PROFILE_H
-
-#define ETL_VERBOSE_ERRORS
-#define ETL_CHECK_PUSH_POP
-
-
-#endif //ETL_PROFILE_H
