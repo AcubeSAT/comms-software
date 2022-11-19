5d4de7062eb882746ed4bc6955cc08bb124c80f0
konkapo <kostaskapogl@gmail.com>
2022-11-19 15:12:22 +0200

    uart test fails, neither of the tasks gets executed

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,7 +6,7 @@
 #include "at86rf215.hpp"
 #include "at86rf215config.hpp"
 #include "FreeRTOSTasks/txUHFTask.hpp"
-
+#include "UARTGatekeeperTask.hpp"
 #include <iostream>
 
 template<class T>
@@ -22,9 +22,9 @@ void uartTask1(void * pvParameters) {
     for(;;)
     {
         ++count1;
-        char str[30];
-        snprintf(str, sizeof("[%d]Task A running\n\r"),"[%d]Task A running\n\r", count1);
-        HAL_UART_Transmit(&huart3, reinterpret_cast<const uint8_t *>(str), sizeof(str), 100);
+        etl::string<LOGGER_MAX_MESSAGE_SIZE> output;
+        output.append("[%d]Task A running\n\r");
+        LOG_DEBUG<<"%d"<< count1 << output.c_str();
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -33,10 +33,11 @@ void uartTask2(void * pvParameters) {
     char count2 = 0;
     for(;;)
     {
+        UART_HandleTypeDef huart2;
         ++count2;
-        char str[30];
-        snprintf(str, sizeof("[%d]Task B running\n\r"),"[%d]Task B running\n\r", count2);
-        HAL_UART_Transmit(&huart3, reinterpret_cast<const uint8_t *>(str), sizeof(str), 100);
+        etl::string<LOGGER_MAX_MESSAGE_SIZE> output;
+        output.append("[%d]Task B running\n\r");
+        LOG_DEBUG<<"%d"<< count2 << output.c_str();
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -44,9 +45,9 @@ void uartTask2(void * pvParameters) {
 void blinkyTask1(void * pvParameters){
     for(;;){
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        HAL_Delay(50);
+        HAL_Delay(200);
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-        HAL_Delay(50);
+        HAL_Delay(200);
     }
 }
 
@@ -61,21 +62,25 @@ namespace AT86RF215 {
 }
 
 extern "C" void main_cpp(){
-
-//    xTaskCreate(uartTask1, "uartTask 1", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
-//    xTaskCreate(uartTask2, "uartTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
+    uartGatekeeperTask.emplace();
+    uartGatekeeperTask->createTask();
+    xTaskCreate(uartTask1, "uartTask 1", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(uartTask2, "uartTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     /**
      * Uncomment below and comment above for Led task visualization (for STM32H743)
      */
-//    xTaskCreate(blinkyTask1, "blinkyTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
-//    xTaskCreate(blinkyTask2, "blinkyTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
-    txUHFTask.emplace(48000, 4800, false);
-
-    txUHFTask->createTask();
+    xTaskCreate(blinkyTask1, "blinkyTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(blinkyTask2, "blinkyTask 2", 1000, NULL, tskIDLE_PRIORITY + 1, NULL);
+//    txUHFTask.emplace(48000, 4800, false);
+//
+//    txUHFTask->createTask();
     vTaskStartScheduler();
 
     for(;;)
+    {
+
+    }
 
     return;
 }
