00bc1de1c180150838727d8ed9906b22c50e949f
Konstantinos Kapoglis <kostaskapogl@gmail.com>
2022-12-11 14:07:28 +0000

    Parameters integration

---
+++ Core/Inc/Platform/Parameters/CommonParameters.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Helpers/Parameter.hpp"
+#include "Platform/COMMS_Definitions.hpp"
+#include "PlatformParameters.hpp"
+
+namespace CommonParameters {
+
+    inline auto &time = PlatformParameters::commsMCUTime;
+
+}
