38d617fa986f8936a5b73bc18f31d6640d67443b
Konstantinos Kapoglis <kostaskapogl@gmail.com>
2022-12-11 14:07:28 +0000

    Parameters integration

---
+++ Core/Inc/Platform/COMMS_Definitions.hpp
@@ -0,0 +1,14 @@
+//
+// Created by konkapo on 24/11/2022.
+//
+
+#ifndef STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
+#define STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
+#include <cstdint>
+#include "Logger_Definitions.hpp"
+/**
+ * Maximum size for a string representation of a service or message type identifier
+ */
+inline const uint8_t MaxTypeIDStringSize = 2;
+
+#endif //STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
