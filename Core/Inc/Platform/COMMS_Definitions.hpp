8016aa7783ff645b87640a7735a1d47f21eeec96
Konstantinos Kapoglis <kostaskapogl@gmail.com>
2023-01-20 12:49:56 +0000

    Uart gatekeeper task

--- Core/Inc/Platform/COMMS_Definitions.hpp
+++ Core/Inc/Platform/COMMS_Definitions.hpp
@@ -1,6 +1,3 @@
-//
-// Created by konkapo on 24/11/2022.
-//
 
 #ifndef STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
 #define STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
@@ -10,5 +7,20 @@
  * Maximum size for a string representation of a service or message type identifier
  */
 inline const uint8_t MaxTypeIDStringSize = 2;
+
+/**
+ * The size of the queue used to communicate with the UART Gatekeeper task
+ */
+inline const uint8_t UARTQueueSize = 5;
+
+/**
+ * Maximum string size for a LogLevel is 9 characters long
+ */
+inline const uint8_t MaxLogNameSize = 9;
+
+/**
+ * TickType_t is a uint32_t number. Its string representation is at most 10 digits long.
+ */
+inline const uint8_t MaxTickCountStringSize = 10;
 
 #endif //STM32H7A3ZIQSETUP_COMMS_DEFINITIONS_H
