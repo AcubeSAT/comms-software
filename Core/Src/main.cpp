a7456b768984b0b9bc8443214e4656e8aab58cda
christina <christina150802@gmail.com>
2023-04-01 17:39:41 +0300

    Minor fixes

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -145,11 +145,13 @@ extern "C" void main_cpp(){
     uartGatekeeperTask->createTask();
     auto output = String<ECSSMaxMessageSize>("New ");
     LOG_DEBUG<<output.c_str();
-    vTaskStartScheduler();
-
 
     mcuTemperatureTask.emplace();
     mcuTemperatureTask->createTask();
+    vTaskStartScheduler();
+
+
+
     
 
     /**
@@ -157,10 +159,7 @@ extern "C" void main_cpp(){
      */
 //    xTaskCreate(blinkyTask1, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
 //    xTaskCreate(blinkyTask2, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
-
-
     for(;;);
-
     return;
 }
 
