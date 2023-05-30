c34561505c439843611ed1a1b57b3d55297076f6
GeorgeKikas <kikasgeorg@ece.auth.gr>
2023-05-30 21:34:48 +0300

    TemperatureSensorTask: Optimize logging parameters

--- Core/Src/FreeRTOSTasks/TemperatureSensorsTask.cpp
+++ Core/Src/FreeRTOSTasks/TemperatureSensorsTask.cpp
@@ -4,13 +4,13 @@
 void TemperatureSensorsTask::execute() {
     auto config = TMP117::Config();
     TMP117::TMP117 tempSensor = TMP117::TMP117(hi2c2, TMP117::I2CAddress::Address1, config);
-    etl::string<5> temperature;
-    etl::format_spec format;
+
     for(;;){
-        etl::pair<TMP117::Error, float> temp = tempSensor.getTemperature(true);
-        if (temp.first == TMP117::Error::NoErrors){
-            etl::to_string(temp.second, temperature, format.precision(2), false);
-            LOG_DEBUG << "Temperature at address " << TMP117::I2CAddress::Address1 << " is " << temperature.c_str();
+        etl::pair<TMP117::Error, float> temperature = tempSensor.getTemperature(true);
+        if (temperature.first == TMP117::Error::NoErrors){
+            Logger::format.precision(2);
+            LOG_DEBUG << "Temperature at address " << TMP117::I2CAddress::Address1 << " is " << temperature.second;
+            PlatformParameters::commsPCBTemperature1.setValue(temperature.second);
         } else {
             LOG_ERROR << "Error getting temperature";
         }
