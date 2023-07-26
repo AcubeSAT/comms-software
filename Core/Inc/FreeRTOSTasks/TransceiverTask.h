caaa2cb73630d2100d9e084b982d81e5e323c27b
christina <christina150802@gmail.com>
2023-07-26 13:21:03 +0300

    implemented queue packet

--- Core/Inc/FreeRTOSTasks/TransceiverTask.h
+++ Core/Inc/FreeRTOSTasks/TransceiverTask.h
@@ -5,20 +5,35 @@
 #include "main.h"
 #include "at86rf215.hpp"
 #include "at86rf215config.hpp"
+#include "queue.h"
+
 extern SPI_HandleTypeDef hspi1;
 
 class TransceiverTask : public Task {
 private:
-    const static inline uint16_t DelayMs = 1000;
-    const static inline uint16_t TaskStackDepth = 2000;
-    const static inline uint8_t LoggerPrecision = 2;
-    AT86RF215::Error error;
+    constexpr static uint16_t DelayMs = 1000;
+    constexpr static uint16_t TaskStackDepth = 2000;
+    constexpr static uint8_t LoggerPrecision = 2;
+    constexpr static uint16_t MaxPacketLength = 64;
 
+//    uint8_t packet[PacketLength] = {0, 1, 2, 3, 43, 56, 2, 78, 12, 34, 82, 39};
+    QueueHandle_t packetQueue;
+    AT86RF215::Error error;
     StackType_t taskStack[TaskStackDepth];
+
 public:
 
     void execute();
-    TransceiverTask() : Task("External Temperature Sensors") {}
+    TransceiverTask() : Task("External Temperature Sensors") {
+        packetQueue = xQueueCreate(MaxPacketLength, sizeof(uint16_t));
+    }
+
+    void addToQueue(const etl::array<uint8_t, MaxPacketLength> &message) {
+        xQueueSendToBack(packetQueue, &message, 0);
+    }
+
+    void createRandomPacket(etl::array<uint8_t, MaxPacketLength> packet, uint16_t length);
+
     AT86RF215::AT86RF215 transceiver = AT86RF215::AT86RF215(&hspi1, AT86RF215::AT86RF215Configuration());
 
     void createTask(){
--- Core/Src/FreeRTOSTasks/TransceiverTask.cpp
+++ Core/Src/FreeRTOSTasks/TransceiverTask.cpp
@@ -1,18 +1,38 @@
 #include "TransceiverTask.h"
 
+/*
+ * This function creates random packets until we have full functionality.
+ */
+void TransceiverTask::createRandomPacket(etl::array<uint8_t, MaxPacketLength> packet, uint16_t length){
+    for (int i=0; i<length; i++){
+        packet[i] = i;
+    }
+}
 
 void TransceiverTask::execute() {
-
 
     transceiver.chip_reset(error);
     transceiver.setup(error);
     volatile uint16_t  b = transceiver.get_version_number(error);
-    uint8_t packet[] = {0, 1, 2, 3, 43, 56, 2, 78, 12, 34, 82, 39};
+
+    uint16_t currentPacketLength = 24;
+    etl::array<uint8_t, MaxPacketLength> packet;
+    createRandomPacket(packet, currentPacketLength);
+
+    for (int i = 0; i < MaxPacketLength; i++) {
+        xQueueSendToBack(packetQueue, &packet[i], portMAX_DELAY);
+    }
+
+    uint8_t receivedPacket[MaxPacketLength];
+    for (int i = 0; i < currentPacketLength; i++) {
+        xQueueReceive(packetQueue, &receivedPacket[i], portMAX_DELAY);
+    }
+
     while (true) {
 
         volatile AT86RF215::State state = transceiver.get_state(AT86RF215::RF09, error);
         state;
-        transceiver.transmitBasebandPacketsTx(AT86RF215::RF09, packet, 12, error);
+        transceiver.transmitBasebandPacketsTx(AT86RF215::RF09, receivedPacket, currentPacketLength, error);
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 
