9d27787a84858dca96e0689ba29d81c0d1877e5d
sourland <jem2010@windowslive.com>
2023-09-05 13:40:31 +0300

    remove unused variable, change stuff to constexpr

--- Core/Inc/FreeRTOSTasks/WatchdogTask.hpp
+++ Core/Inc/FreeRTOSTasks/WatchdogTask.hpp
@@ -13,25 +13,19 @@ private:
     * @brief Counter clock prescaler value.
     * Represents the division factor applied to the IWDG clock.
     */
-    const uint16_t CounterClockPrescaler = 128;
+    static constexpr uint16_t CounterClockPrescaler = 128;
 
     /**
     * @brief Window value for IWDG.
     * This value defines the window in which the counter can be refreshed.
     */
-    const uint16_t WindowValue = 4095;
-
-    /**
-    * @brief Down-counter reload value.
-    * The counter counts down from this value. When it reaches zero, a reset is triggered.
-    */
-    const uint16_t DownCounterReloadValue = 4095;
+    static constexpr uint16_t WindowValue = 4095;
 
     /**
     * @brief Clock frequency for the IWDG.
     * Represents the base frequency before applying any prescaler.
     */
-    const uint16_t ClockFrequency = 32000;
+    static constexpr uint16_t ClockFrequency = 32000;
 
     /**
      * @brief Calculates and stores the window time in milliseconds.
@@ -40,7 +34,7 @@ private:
      * be refreshed to prevent a reset. It's calculated using the window value, counter
      * clock prescaler, and the clock frequency.
      */
-    const uint16_t WindowTime = 1000 * (WindowValue * CounterClockPrescaler) / ClockFrequency;
+    static constexpr uint16_t WindowTime = 1000 * (WindowValue * CounterClockPrescaler) / ClockFrequency;
 
     const static inline uint16_t TaskStackDepth = 1000;
 
