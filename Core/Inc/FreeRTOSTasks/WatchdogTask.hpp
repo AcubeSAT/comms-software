54e7275ebfc02f5d634aa9b60b202dadcdffba25
sourland <jem2010@windowslive.com>
2023-09-05 12:49:15 +0300

    fix docs, remove irrelevant docs

--- Core/Inc/FreeRTOSTasks/WatchdogTask.hpp
+++ Core/Inc/FreeRTOSTasks/WatchdogTask.hpp
@@ -13,25 +13,25 @@ private:
     * @brief Counter clock prescaler value.
     * Represents the division factor applied to the IWDG clock.
     */
-    const uint16_t CounterClockPrescaler = 0x0080;
+    const uint16_t CounterClockPrescaler = 128;
 
     /**
     * @brief Window value for IWDG.
-    * This value defines the window in which the counter can be updated.
+    * This value defines the window in which the counter can be refreshed.
     */
-    const uint16_t WindowValue = 0x0800;
+    const uint16_t WindowValue = 4095;
 
     /**
     * @brief Down-counter reload value.
     * The counter counts down from this value. When it reaches zero, a reset is triggered.
     */
-    const uint16_t DownCounterReloadValue = 0x1000;
+    const uint16_t DownCounterReloadValue = 4095;
 
     /**
     * @brief Clock frequency for the IWDG.
     * Represents the base frequency before applying any prescaler.
     */
-    const uint16_t ClockFrequency = 0x7D00;
+    const uint16_t ClockFrequency = 32000;
 
     /**
      * @brief Calculates and stores the window time in milliseconds.
@@ -41,13 +41,6 @@ private:
      * clock prescaler, and the clock frequency.
      */
     const uint16_t WindowTime = 1000 * (WindowValue * CounterClockPrescaler) / ClockFrequency;
-
-    /**
-    * The delay in milliseconds between each watchdog reset. It is set to 8000ms, since the
-    * maximum timeout is 16000ms, so the timer needs to clear before the next reset.
-    * @see main.h for the specific configuration.
-    * Do not change this value, without also modifying the value in STM32CubeMX
-    */
 
     const static inline uint16_t TaskStackDepth = 1000;
 
