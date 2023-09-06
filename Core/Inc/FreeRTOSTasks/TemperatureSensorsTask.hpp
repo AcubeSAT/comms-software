a66a0439cea7e8ec6e77af33522b6c42a2888eae
sourland <jem2010@windowslive.com>
2023-09-06 17:01:46 +0300

    change class layout

--- Core/Inc/FreeRTOSTasks/TemperatureSensorsTask.hpp
+++ Core/Inc/FreeRTOSTasks/TemperatureSensorsTask.hpp
@@ -7,23 +7,24 @@
 extern I2C_HandleTypeDef hi2c2;
 
 class TemperatureSensorsTask : public Task {
-private:
-    const static inline uint16_t DelayMs = 1000;
-    const static inline uint16_t TaskStackDepth = 2000;
-    const static inline uint8_t LoggerPrecision = 2;
-
-    StackType_t taskStack[TaskStackDepth];
 public:
 
     void execute();
 
     TemperatureSensorsTask() : Task("External Temperature Sensors") {}
 
-    void createTask(){
-        xTaskCreateStatic(vClassTask<TemperatureSensorsTask>, this->TaskName,
+    void createTask() {
+        xTaskCreateStatic(vClassTask < TemperatureSensorsTask > , this->TaskName,
                           TemperatureSensorsTask::TaskStackDepth, this, tskIDLE_PRIORITY + 1,
                           this->taskStack, &(this->taskBuffer));
     }
+
+private:
+    const static inline uint16_t DelayMs = 1000;
+    const static inline uint16_t TaskStackDepth = 2000;
+    const static inline uint8_t LoggerPrecision = 2;
+
+    StackType_t taskStack[TaskStackDepth];
 };
 
 inline std::optional<TemperatureSensorsTask> temperatureSensorsTask;
