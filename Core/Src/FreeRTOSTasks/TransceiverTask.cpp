10e6f4e85fa9f0b4349419eda5ba5cd58d41101c
Andronikos Kostas <andronikostaslos@gmail.com>
2023-10-31 13:11:44 +0200

    create a random packet

---
+++ Core/Src/FreeRTOSTasks/TransceiverTask.cpp
@@ -0,0 +1,53 @@
+
+#include "TransceiverTask.hpp"
+
+AT86RF215::AT86RF215 TransceiverTask::transceiver = AT86RF215::AT86RF215(&hspi1, AT86RF215::AT86RF215Configuration());
+
+
+TransceiverTask::PacketType TransceiverTask::createRandomPacket(uint16_t length) {
+    PacketType packet;
+    for (std::size_t i = 0; i < length; i++) {
+        packet[i] = i;
+    }
+    return packet;
+}
+
+void TransceiverTask::setConfiguration(uint16_t pllFrequency09, uint8_t pllChannelNumber09) {
+    configFrequency.pllFrequency09 = pllFrequency09;
+    configFrequency.pllChannelNumber09 = pllChannelNumber09;
+    configFrequency.pllChannelMode09 = AT86RF215::PLLChannelMode::FineResolution450;
+    transceiver.config = configFrequency;
+}
+
+/*
+* The frequency cannot be lower than 377000 as specified in section 6.3.2. The frequency range related
+* to Fine Resolution Channel Scheme CNM.CM=1 is from 389.5MHz to 510MHz
+*/
+uint16_t TransceiverTask::calculatePllChannelFrequency09(uint32_t frequency) {
+    uint32_t N = (frequency - 377000) * 65536 / 6500;
+    return N >> 8;
+}
+
+/*
+* The frequency cannot be lower than 377000 as specified in section 6.3.2. The frequency range related
+* to Fine Resolution Channel Scheme CNM.CM=1 is from 389.5MHz to 510MHz
+*/
+uint8_t TransceiverTask::calculatePllChannelNumber09(uint32_t frequency) {
+    uint32_t N = (frequency - 377000) * 65536 / 6500;
+    return N & 0xFF;
+}
+
+void TransceiverTask::execute() {
+    setConfiguration(calculatePllChannelFrequency09(FrequencyUHF), calculatePllChannelNumber09(FrequencyUHF));
+
+    transceiver.chip_reset(error);
+    transceiver.setup(error);
+
+    uint16_t currentPacketLength = 44;
+    PacketType packet = createRandomPacket(currentPacketLength);
+
+    while (true) {
+        transceiver.transmitBasebandPacketsTx(AT86RF215::RF09, packet.data(), currentPacketLength, error);
+        vTaskDelay(pdMS_TO_TICKS(DelayMs));
+    }
+}
