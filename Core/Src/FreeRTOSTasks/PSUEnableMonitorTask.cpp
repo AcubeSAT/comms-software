7b3f0e560951e7d7cc48d77440781c85eef23b44
ntoylker <nikos.toulk@gmail.com>
2024-01-16 17:05:29 +0200

    Change the logger messages and the way the PG faults are dealt with

--- Core/Src/FreeRTOSTasks/PSUEnableMonitorTask.cpp
+++ Core/Src/FreeRTOSTasks/PSUEnableMonitorTask.cpp
@@ -14,21 +14,31 @@ extern PSU::solve_PG_fault(); */
 void PSUEnableMonitorTask::execute() {
 
     // enable the different parts of PSU IF disabled
-    if (HAL_GPIO_ReadPin(P5V_FPGA_EN_GPIO_Port, P5V_FPGA_EN_Pin) == GPIO_PIN_RESET )
-        PSU::PSU::enable_FPGA_PSU();
-    if (HAL_GPIO_ReadPin(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin) == GPIO_PIN_RESET )
-        PSU::PSU::enable_RF_PSU();
+    if (PSU::PSU::isOff(P5V_FPGA_EN_GPIO_Port, P5V_FPGA_EN_Pin))
+        PSU::PSU::enablePartPSU(P5V_FPGA_EN_GPIO_Port, P5V_FPGA_EN_Pin);
+    if (PSU::PSU::isOff(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin))
+        PSU::PSU::enablePartPSU(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin);
 
-    // the periodic(5 sec) monitoring of PG signals
-    while(1) {
-        if(PSU::PSU::PG_read())
-            LOG_INFO << "ALL 3 PG signals are in SET state (P5V_FPGA, P5V_RF, P3V3_RF)";
-        else {
-            LOG_INFO << "1 or more PG signals are NOT in SET state (P5V_FPGA, P5V_RF, P3V3_RF)";
-            LOG_INFO << "CALLING FUNCTION TO SOLVE IT";
-            PSU::PSU::solve_PG_fault();
+    HAL_Delay(200); // Give Some Time for the Signals to settle in I guess
+    if (PSU::PSU::PG_read())
+        LOG_INFO << "ALL 3 PG signals are in SET state (P5V_FPGA, P5V_RF, P3V3_RF)";
+
+    // the periodic(3 sec) monitoring of PG signals
+    while (1) {
+        if(PSU::PSU::isOff(P5V_FPGA_PG_GPIO_Port, P5V_FPGA_PG_Pin)) {
+            LOG_INFO << "P5V_FPGA PG signal is NOT in SET state";
+            PSU::PSU::solvePGfault(P5V_FPGA_PG_GPIO_Port, P5V_FPGA_PG_Pin);
         }
-        HAL_Delay(5000);
+        if(PSU::PSU::isOff(P5V_RF_PG_GPIO_Port, P5V_RF_PG_Pin)) {
+            LOG_INFO << "P5V_RF PG signal is NOT in SET state";
+            PSU::PSU::solvePGfault(P5V_RF_PG_GPIO_Port, P5V_RF_PG_Pin);
+        }
+        if(PSU::PSU::isOff(P3V3_RF_PG_GPIO_Port, P3V3_RF_PG_Pin)) {
+            LOG_INFO << "P3V3_RF PG signal is NOT in SET state";
+            PSU::PSU::solvePGfault(P3V3_RF_PG_GPIO_Port, P3V3_RF_PG_Pin);
+        }
+
+        HAL_Delay(3000);
     }
 
 }
