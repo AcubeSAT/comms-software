940187156a76eca0d6d988f5c47c149c88c5f0f5
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2024-01-19 20:38:32 +0200

    Added static casts to int so that enums are displayed correctly

--- Core/Src/FreeRTOSTasks/TemperatureSensorsTask.cpp
+++ Core/Src/FreeRTOSTasks/TemperatureSensorsTask.cpp
@@ -1,91 +1,37 @@
 #include "TemperatureSensorsTask.hpp"
 #include "TMP117.hpp"
 
-etl::string<TemperatureSensorsTask::maxErrorStringSize> TemperatureSensorsTask::errorString(TMP117::Error error){
-    switch(error) {
-        case TMP117::NoErrors:
-            return "NoErrors";
-        case TMP117::Timeout:
-            return "Timeout";
-        case TMP117::InvalidEEPROM:
-            return "InvalidEEPROM";
-        case TMP117::TemperatureHigh:
-            return "TemperatureHigh";
-        case TMP117::TemperatureLow:
-            return "TemperatureLow";
-        case TMP117::NoDataReady:
-            return "NoDataReady";
-        case TMP117::InvalidCalibrationOffset:
-            return "InvalidCalibrationOffset";
-        default:
-            break;
-    }
-
-    return "UnknownError";
-}
-
-etl::string<TemperatureSensorsTask::maxSensorNameSize> TemperatureSensorsTask::sensorName(TMP117::I2CAddress slaveAddress){
-    switch(slaveAddress) {
-        case TMP117::I2CAddress::Address3:
-            return "PCB MCU";
-        case TMP117::I2CAddress::Address1:
-            return "PCB UHF PA";
-        case TMP117::I2CAddress::Address4:
-            return "PCB SBAND PA";
-        default:
-            break;
-    }
-
-    return "UNKNOWN ADDRESS";
-}
-
-
 /**
- * Actual execute method
+ * Actual execute() method
  */
- /*
+/*
 void TemperatureSensorsTask::execute() {
+    auto config = TMP117::Config();
+    TMP117::TMP117 tempSensor = TMP117::TMP117(hi2c2, TMP117::I2CAddress::Address1, config);
 
-    etl::array<sensor, 3> sensors = {
-        sensor {TMP117::TMP117(hi2c2, TMP117::I2CAddress::Address3, config),
-                         PlatformParameters::commsPCBTemperatureMCU,
-                         sensorName(TMP117::Address3)},
-        sensor {TMP117::TMP117(hi2c2, TMP117::I2CAddress::Address1, config),
-                         PlatformParameters::commsPCBTemperatureUHF,
-                         sensorName(TMP117::I2CAddress::Address1)},
-        sensor {TMP117::TMP117(hi2c2, TMP117::I2CAddress::Address4, config),
-                         PlatformParameters::commsPCBTemperatureSBAND,
-                         sensorName(TMP117::I2CAddress::Address4)},
-    };
-
-    // for temporarily holding of a temperature and error
-    etl::pair<TMP117::Error, float> temperature;
-
-    while (true) {
-        Logger::format.precision(LoggerPrecision);
-        for (auto s : sensors) {
-            temperature = s.sensorObject.getTemperature(true);
-            if (temperature.first == TMP117::Error::NoErrors) {
-                LOG_DEBUG << "Temperature at " << s.sensorName->data() << ": " << temperature.second;
-                s.platformParameterReference.setValue(temperature.second);
-            } else {
-                LOG_ERROR << "Could not get temperature at " << s.sensorName->data() << ". Error: "
-                          << errorString(temperature.first).data();
-            }
+    while(true){
+        etl::pair<TMP117::Error, float> temperature = tempSensor.getTemperature(true);
+        if (temperature.first == TMP117::Error::NoErrors){
+            Logger::format.precision(LoggerPrecision);
+            LOG_DEBUG << "Temperature at address " << TMP117::I2CAddress::Address1 << " is " << temperature.second;
+            PlatformParameters::commsPCBTemperature1.setValue(temperature.second);
+        } else {
+            LOG_ERROR << "Error getting temperature";
         }
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 }
 */
 
+
 /** Tests the methods: readRegister(),writeRegister()
  *  Writes a specified value to a specified register.It is expected that the read value is the same as the specified value
 **/
-
+/*
 void TemperatureSensorsTask::execute() {
     TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
 
-    uint16_t writeValue = 100;
+    uint16_t writeValue = 99;
     TMP117::RegisterAddress targetRegister = TMP117::RegisterAddress::TemperatureHighLimit;
 
     TMP117::Error writeError;
@@ -95,13 +41,13 @@ void TemperatureSensorsTask::execute() {
 
         writeError = sensor.writeRegister(targetRegister,writeValue);
         readPair = sensor.readRegister(targetRegister);
-        LOG_DEBUG << "Write Error: " << writeError << "\r\n";
-        LOG_DEBUG << "Read Error: " << readPair.first << "\r\n";
+        LOG_DEBUG << "Write Error: " << static_cast<int16_t>(writeError) << "\r\n";
+        LOG_DEBUG << "Read Error: " << static_cast<int16_t>(readPair.first) << "\r\n";
         LOG_DEBUG << "Expected: " << writeValue << " got: " << (readPair.second.has_value() ? readPair.second.value() : -9999) << "\r\n";
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 }
-
+*/
 
 /** Tests the methods: configure()
  * Reads the configuration register (only the 11 least significant bits concern settings) and compares it with
@@ -123,12 +69,12 @@ void TemperatureSensorsTask::execute() {
                 (sensor.configuration.thermalAlert << 4) |
                 (sensor.configuration.polarityAlert << 3) |
                 (sensor.configuration.drAlert << 2);
-        LOG_DEBUG << "Read Error: " << readPair.first << "\r\n";
+        LOG_DEBUG << "Read Error: " << static_cast<int16_t>(readPair.first) << "\r\n";
         LOG_DEBUG << "Expected: " << configVal << " got: " << (readPair.second.has_value() ? (readPair.second.value() & 0x7FF): -9999) << "\r\n";
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 }
- */
+*/
 
 /** Tests the methods: readEEPROM(),writeEEPROM()
  * To test if a value is indeed written to EEPROM (and not just the volatile register),the following steps are performed:
@@ -140,7 +86,6 @@ void TemperatureSensorsTask::execute() {
  * 4. Read the registers again.It is expected that only EEPROM1 will show the new value.
  *
 **/
-/*
 void TemperatureSensorsTask::execute() {
     TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
     TMP117::RegisterAddress targetedEEPROM1 = TMP117::RegisterAddress::EEPROM1;
@@ -151,40 +96,41 @@ void TemperatureSensorsTask::execute() {
         //First,read the preexisting values.
         auto preRead1 = sensor.readEEPROM(1);
         auto preRead2 = sensor.readEEPROM(2);
-        LOG_DEBUG << "pre-read errors: " << preRead1.first << " and " << preRead1.first;
-        LOG_DEBUG << "Preexisting values: " << preRead1.second.value() << " and " << preRead1.second.value();
+        LOG_DEBUG << "pre-read errors: " << static_cast<int16_t>(preRead1.first) << " and " << static_cast<int16_t>(preRead2.first) << "\r\n";
+        LOG_DEBUG << "Preexisting values: " << preRead1.second.value() << " and " << preRead2.second.value() << "\r\n";
 
         //Write new values (with writeEEPROM in the first register and writeRegister in the second)
-        uint16_t val1 = 89;
-        uint16_t val2 = 139;
+        uint16_t val1 = rand()%100;
+        uint16_t val2 = rand()%100;
+        LOG_DEBUG << "Values that are supposed to be written:" << val1 << " and " << val2 << "\r\n";
+
         auto write1 = sensor.writeEEPROM(1,val1);
-        // Ensure that EEPROM is locked
-        LOG_DEBUG << "EUN bit is:" << (sensor.readRegister(TMP117::EEPROMUnlock).second.value() & 0x8000);
+        // Ensure that EEPROM is locked after method ends
+        LOG_DEBUG << "EUN bit is:" << ((sensor.readRegister(TMP117::EEPROMUnlock).second.value() & 0x8000) >> 15) << "\r\n";
         auto write2 = sensor.writeRegister(targetedEEPROM2,val2);
-        LOG_DEBUG << "write errors: " << write1 << " and " << write2;
+        LOG_DEBUG << "write errors: " << static_cast<int16_t>(write1) << " and " << static_cast<int16_t>(write2) << "\r\n";
 
-        //Issue a soft reset (that should clean the current values of the registers,making them to load whatever they
+        // Issue a soft reset (that should clean the current values of the registers,making them load whatever they
         // have in the actual EEPROM)
-        etl::pair<TMP117::Error,std::optional<uint16_t>> currentVal = sensor.readRegister(TMP117::RegisterAddress::ConfigurationRegister);
-        sensor.writeRegister(TMP117::RegisterAddress::ConfigurationRegister, currentVal.second.value() | 0x2);
-        sensor.configure();
+       etl::pair<TMP117::Error,std::optional<uint16_t>> currentVal = sensor.readRegister(TMP117::RegisterAddress::ConfigurationRegister);
+       sensor.writeRegister(TMP117::RegisterAddress::ConfigurationRegister, currentVal.second.value() | 0x2);
+       HAL_Delay(3);
+       sensor.configure();
 
-        //Read the registers again.Only the one that was read written with writeEEPROM should hold its new value.
+        //Read the registers again.Only the one that was read written with writeEEPROM should hold it's new value.
         auto afterRead1 = sensor.readEEPROM(1);
         auto afterRead2 = sensor.readEEPROM(2);
-        LOG_DEBUG << "after-read errors: " << afterRead1.first << " and " << afterRead1.first;
-        LOG_DEBUG << "Final values: " << preRead1.second.value() << " and " << preRead1.second.value();
-        LOG_DEBUG << "Expected" << val1 << " and " << val2;
+        LOG_DEBUG << "after-read errors: " << static_cast<int16_t>(afterRead1.first) << " and " << static_cast<int16_t>(afterRead2.first) << "\r\n";
+        LOG_DEBUG << "Final values: " << preRead1.second.value() << " and " << preRead2.second.value() << "\r\n";
 
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 }
-*/
 
 /** Tests the methods: getRevNumber()
  *  Just ensuring that something is returned.
  **/
- /*
+/*
 void TemperatureSensorsTask::execute() {
     TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
 
@@ -192,7 +138,7 @@ void TemperatureSensorsTask::execute() {
     while (true) {
         Logger::format.precision(LoggerPrecision);
         readPair = sensor.getRevNumber();
-        LOG_DEBUG << "Read Error: " << readPair.first << "\r\n";
+        LOG_DEBUG << "Read Error: " << static_cast<int16_t>(readPair.first) << "\r\n";
         LOG_DEBUG <<  "Got: " << ((readPair.first== TMP117::NoErrors) ? readPair.second: -9999) << "\r\n";
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
@@ -202,7 +148,7 @@ void TemperatureSensorsTask::execute() {
 /** Tests the methods: getDeviceID()
  *  Same as test method for getRevNumber()
  **/
- /*
+/*
 void TemperatureSensorsTask::execute() {
     TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
 
@@ -210,30 +156,13 @@ void TemperatureSensorsTask::execute() {
     while (true) {
         Logger::format.precision(LoggerPrecision);
         readPair = sensor.getDeviceID();
-        LOG_DEBUG << "Read Error: " << readPair.first << "\r\n";
+        LOG_DEBUG << "Read Error: " << static_cast<int16_t>(readPair.first) << "\r\n";
         LOG_DEBUG <<  "Got: " << ((readPair.first== TMP117::NoErrors) ? readPair.second: -9999) << "\r\n";
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
 }
-*/
+ */
 
-/** Tests the methods: getDeviceID()
- *  Same as test method for getRevNumber()
- **/
- /*
-void TemperatureSensorsTask::execute() {
-    TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
-
-    etl::pair<TMP117::Error, uint16_t> readPair;
-    while (true) {
-        Logger::format.precision(LoggerPrecision);
-        readPair = sensor.getDeviceID();
-        LOG_DEBUG << "Read Error: " << readPair.first << "\r\n";
-        LOG_DEBUG <<  "Got: " << ((readPair.first== TMP117::NoErrors) ? readPair.second: -9999) << "\r\n";
-        vTaskDelay(pdMS_TO_TICKS(DelayMs));
-    }
-}
-*/
 
 /** Tests the methods: setCalibrationOffset(),getCalibrationOffset()
  *  Test different calibration offsets,positive and negative.If |offset|>256, setCalibrationOffset should
@@ -243,15 +172,15 @@ void TemperatureSensorsTask::execute() {
 void TemperatureSensorsTask::execute() {
     TMP117::TMP117 sensor = TMP117::TMP117(hi2c2,TMP117::I2CAddress::Address1,config);
 
-    float writeValue = 0;
-    etl::pair<TMP117::Error, std::optional<float>> CalibPair;
+    float writeValue = -78;
+    etl::pair<TMP117::Error, std::optional<float>> calibPair;
     etl::pair<TMP117::Error, float> readPair;
     while (true) {
         Logger::format.precision(LoggerPrecision);
-        CalibPair = sensor.setCalibrationOffset(writeValue);
+        calibPair = sensor.setCalibrationOffset(writeValue);
         readPair = sensor.getCalibrationOffset();
-        LOG_DEBUG << "Set Calibration Error: " << CalibPair.first << "\r\n";
-        LOG_DEBUG << "Read Calibration Error: " << readPair.first << "\r\n";
+        LOG_DEBUG << "Set Calibration Error: " << static_cast<int>(calibPair.first) << "\r\n";
+        LOG_DEBUG << "Read Calibration Error: " << static_cast<int16_t>(readPair.first) << "\r\n";
         LOG_DEBUG << "Expected: " << writeValue <<" Got: " << ((readPair.first== TMP117::NoErrors) ? readPair.second: -9999) << "\r\n";
         vTaskDelay(pdMS_TO_TICKS(DelayMs));
     }
@@ -278,14 +207,14 @@ void TemperatureSensorsTask::execute() {
         Logger::format.precision(LoggerPrecision);
         temperature = sensor.getTemperature(true);
         LOG_DEBUG << "getTemperatureValues:\r\n";
-        LOG_DEBUG << "Error: " << temperature.first << " Temperature: " << temperature.second << "\r\n";
+        LOG_DEBUG << "Error: " << static_cast<int16_t>(temperature.first) << " Temperature: " << temperature.second << "\r\n";
 
 
         offset = sensor.getCalibrationOffset();
         temp = sensor.readRegister(TMP117::RegisterAddress::TemperatureRegister);
         LOG_DEBUG << "Manually read values:\r\n";
-        LOG_DEBUG << "Calibration Offset reading errors: " << offset.first << "\r\n";
-        LOG_DEBUG << "Temp reading errors: " << temp.first << "\r\n";
+        LOG_DEBUG << "Calibration Offset reading errors: " << static_cast<int16_t>(offset.first) << "\r\n";
+        LOG_DEBUG << "Temp reading errors: " << static_cast<int16_t>(temp.first) << "\r\n";
         LOG_DEBUG << "Temp obtained: " << sensor.convertTemperature(temp.second.value()) + offset.second << "\r\n";
 
 
