414bb5943e2acdd92d20402544364c880b8ae39c
ntoylker <nikos.toulk@gmail.com>
2024-01-27 19:43:17 +0200

    change the task's delay() functions amongst other small things

--- Core/Src/FreeRTOSTasks/PSUEnableMonitorTask.cpp
+++ Core/Src/FreeRTOSTasks/PSUEnableMonitorTask.cpp
@@ -1,9 +1,6 @@
 #include "FreeRTOSTasks/PSUEnableMonitorTask.hpp"
 #include "Task.hpp"
-
-#include "stm32h7xx_hal_gpio.h"
 #include "PSU.hpp"
-//#include "../lib/component-drivers/PSU/src/PSU.cpp"
 
 PSU psu = PSU( GPIOD, GPIO_PIN_9
         , GPIOC, GPIO_PIN_7
@@ -15,9 +12,9 @@ void PSUEnableMonitorTask::execute() {
 
     // 1st enable the different parts of PSU
     psu.enablePartPSU(psu.p5vFPGAenPORT, psu.p5vFPGAenPIN);
-    HAL_Delay(200); // settle time
+    vTaskDelay(pdMS_TO_TICKS(1000)); // settle time
     psu.enablePartPSU(psu.p5vRFenPORT, psu.p5vRFenPIN);
-    HAL_Delay(200); // settle time
+    vTaskDelay(pdMS_TO_TICKS(1000)); // settle time
 
     if (psu.PGread())
         LOG_INFO << "ALL 3 PG signals are in SET state (P5V_FPGA, P5V_RF, P3V3_RF)";
@@ -32,12 +29,12 @@ void PSUEnableMonitorTask::execute() {
             LOG_INFO << "P5V_RF PG signal is NOT in SET state";
             psu.solvePGfault(psu.p5vRFpgPORT, psu.p5vRFpgPIN);
         }
-        if(psu.isPinOff(psu.p3v3RFenPORT, psu.p3v3RFpgPIN)) {
+        if(psu.isPinOff(psu.p3v3RFpgPORT, psu.p3v3RFpgPIN)) {
             LOG_INFO << "P3V3_RF PG signal is NOT in SET state";
-            psu.solvePGfault(psu.p3v3RFenPORT, psu.p3v3RFpgPIN);
+            psu.solvePGfault(psu.p3v3RFpgPORT, psu.p3v3RFpgPIN);
         }
 
-        HAL_Delay(3000);
+        vTaskDelay(pdMS_TO_TICKS(3000));
     }
 
 }
