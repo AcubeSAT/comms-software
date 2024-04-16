6ca2cd0d3e86955fe968bb18aab9eb3cad827750
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2024-04-16 21:29:50 +0300

    Remove settings from task

--- Core/Src/FreeRTOSTasks/TransceiverTask.cpp
+++ Core/Src/FreeRTOSTasks/TransceiverTask.cpp
@@ -24,71 +24,19 @@ TransceiverTask::PacketType TransceiverTask::createRandomPacket(uint16_t length)
 }
 
 void TransceiverTask::setConfiguration(uint16_t pllFrequency09, uint8_t pllChannelNumber09) {
-    // Sub GHz radio and BBC0
-    //     General Settings
-    customConfig.pllFrequency09 = pllFrequency09;
-    customConfig.pllChannelNumber09 = pllChannelNumber09;
-    customConfig.pllChannelMode09 = AT86RF215::PLLChannelMode::FineResolution450;
-    customConfig.continuousTransmit09 = false;
-    customConfig.baseBandEnable09 = true;
-    customConfig.physicalLayerType09 = AT86RF215::PhysicalLayerType::BB_MRFSK;
-
-    //     Receiver frontend
-    customConfig.ifInversion09 = false;
-    customConfig.ifShift09 = false;
-    customConfig.rxBandwidth09 = AT86RF215::ReceiverBandwidth::RF_BW200KHZ_IF250KHZ;
-    customConfig.rxRelativeCutoffFrequency09 = AT86RF215::RxRelativeCutoffFrequency::FCUT_0375;
-    //      Set to FS_400 for v.1 transceiver (X-PRO) and FS_500 for v.3 transceiver (X-PRO-A)
-   // if (transceiver.get_version_number(error) == 3)
-   //     customConfig.receiverSampleRate09 = AT86RF215::ReceiverSampleRate::FS_500;
-   // else
-        customConfig.receiverSampleRate09 = AT86RF215::ReceiverSampleRate::FS_400;
-
-    //     AGC
-    customConfig.agcEnabled09 = true;
-    customConfig.agcInput09 = false;
-    customConfig.averageTimeNumberSamples09 = AT86RF215::AverageTimeNumberSamples::AVGS_8;
-    customConfig.automaticGainControlTarget09 = AT86RF215::AutomaticGainTarget::DB30;
-    customConfig.gainControlWord09 = 23;
-
-    //     RSSI
-    customConfig.energyDetectionMode09 = AT86RF215::EnergyDetectionMode::RF_EDAUTO;
-    customConfig.energyDetectFactor09 = 16;
-    customConfig.energyDetectionBasis09 = AT86RF215::EnergyDetectionTimeBasis::RF_8MS;
-
-    //     Tx frontend
-    customConfig.txRelativeCutoffFrequency09 = AT86RF215::TxRelativeCutoffFrequency::FCUT_0375;
-    customConfig.transceiverSampleRate09 = AT86RF215::TransmitterSampleRate::FS_400;
-    customConfig.powerAmplifierRampTime09 = AT86RF215::PowerAmplifierRampTime::RF_PARAMP32U;
-    customConfig.transmitterCutOffFrequency09 = AT86RF215::TransmitterCutOffFrequency::RF_FLC80KHZ;
-    customConfig.txOutPower09 = 0x0;  // Each increment adds 1dBm,max value is 0x1F
-
-    //     Direct modulation and pre-emphasis filter
-    //     FEATURE AVAILABLE ONLY IN V.3 TRANSCEIVER (X-PRO-A DEVBOARD)
-    if (transceiver.get_version_number(error) == 3) {
-        customConfig.directModulation09 = false;
-        customConfig.enablePE09 = false;
-        customConfig.configPE0_09 = 0x2;
-        customConfig.configPE1_09 = 0x3;
-        customConfig.configPE2_09 = 0xFC;
-    }
-
     //     FSK modulation
     //     BT = 1 , MIDXS = 1, MIDX = 1, MOR = B-FSK
     transceiver.spi_write_8(AT86RF215::BBC0_FSKC0, 86, error);
 
     //     FCS and interleaving
     uint8_t reg = transceiver.spi_read_8(AT86RF215::BBC0_PC, error);
-//    //         ENABLE TXSFCS (FCS autonomously calculated)
+    //         ENABLE TXSFCS (FCS autonomously calculated)
     transceiver.spi_write_8(AT86RF215::BBC0_PC, reg | (1 << 4), error);
-//    //         ENABLE FCS FILTER
+    //         ENABLE FCS FILTER
     transceiver.spi_write_8(AT86RF215::BBC0_PC, reg | (1 << 6), error);
     reg = transceiver.spi_read_8(AT86RF215::BBC0_FSKC2, error);
-//    //         DISABLE THE INTERLEAVING
+    //         DISABLE THE INTERLEAVING
     transceiver.spi_write_8(AT86RF215::BBC0_PC, reg & 0, error);
-
-
-    // 2.4 GHz radio and BBC1
 
     transceiver.config = customConfig;
 }
@@ -139,19 +87,18 @@ void TransceiverTask::execute() {
             LOG_DEBUG << "Energy (RSSI register): " << transceiver.get_rssi(AT86RF215::RF09,error) << "\n";
         **/
 
-        bool rx = false;  // Switch to false for tx.Could make that value change via pressing a button or periodically,
-                         // to simulate half duplex communication
+        bool rx = true;  // true: Rx  false: Tx
         if (rx) {
             /**RXFS,RSFE interrupts and packet reception**/
             transceiver.transmitBasebandPacketsRx(AT86RF215::RF09, error); // Sets the tranceiver to state RX
             vTaskDelay(pdMS_TO_TICKS(100));      // Wait for handle_irq() to detect the interrupts and read the packets
             if (!transceiver.got_stateRX)
                 LOG_DEBUG << "Could not get to state rx\n";   // Try fiddling with the delay above,if state RX cannot be reached
-            else {         // Flag variables got_rxfs,got_rxfe determine if interrupts occurred
+            else {
                 LOG_DEBUG << "Entered state rx\n";
                 transceiver.got_stateRX = false;
             }
-            if (transceiver.got_rxfs)
+            if (transceiver.got_rxfs)  // Flag variables got_rxfs,got_rxfe signal if interrupts occurred
                 LOG_DEBUG << "Got rxfs\n";
                 transceiver.got_rxfs = false;
             if (transceiver.got_rxfe) {
