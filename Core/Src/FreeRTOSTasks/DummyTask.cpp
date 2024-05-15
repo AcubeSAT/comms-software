5106a783c0a8a171cb0213f8f756ba979a4c1fc9
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2024-05-15 17:40:32 +0300

    Clean up main.cpp, remove blinky tasks.

--- Core/Src/FreeRTOSTasks/DummyTask.cpp
+++ Core/Src/FreeRTOSTasks/DummyTask.cpp
@@ -4,7 +4,7 @@
 
 void DummyTask::execute() {
     for(;;){
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+        HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
         HAL_Delay(300);
     }
 }
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -3,52 +3,13 @@
 #include "list.h"
 #include "task.h"
 #include "InitializationTask.hpp"
-#include "at86rf215.hpp"
-#include "at86rf215config.hpp"
-
-extern SPI_HandleTypeDef hspi1;
-extern UART_HandleTypeDef huart3;
-extern I2C_HandleTypeDef hi2c2;
-extern RTC_HandleTypeDef hrtc;
-
-template<class T>
-static void vClassTask(void *pvParameters) {
-    (static_cast<T *>(pvParameters))->execute();
-}
-
-void blinkyTask1(void * pvParameters){
-    for(;;){
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        HAL_Delay(50);
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-        HAL_Delay(50);
-    }
-}
-
-void blinkyTask2(void * pvParameters){
-    for(;;){
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        HAL_Delay(300);
-    }
-}
-
-namespace AT86RF215 {
-    AT86RF215 transceiver = AT86RF215(&hspi1, AT86RF215Configuration());
-}
 
 extern "C" void main_cpp(){
 
     initializationTask.emplace();
-
     initializationTask->createTask();
-
     vTaskStartScheduler();
 
-    /**
-     * Uncomment below and comment above for Led task visualization (for STM32H743)
-     */
-//    xTaskCreate(blinkyTask1, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
-//    xTaskCreate(blinkyTask2, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
     for(;;);
     return;
 }
